b6fbeb46b6d6108f7dd0808b8c18c644762559d3
Rodrigo <r.barboza9502@gmail.com>
2024-02-21 15:23:48 -0300

    deploy ready to netlify

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,1 +1,4 @@
 export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID ?? "myclientid";
+export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET ?? "myclientsecret";
